afc0324e3daaada8043e4fcff34130d57ab80ef1
attix-zhang <attix.zhang.cs@gmail.com>
2019-09-18 20:02:12 -0500

    Add test code for open_memstream.

---
+++ stdio/open_memstream.c
@@ -0,0 +1,22 @@
+// gcc -o open_memstream open_memstream.c -I ../include/ -L ../lib/ -lapue
+#include "apue.h"
+
+int main() {
+    int i;
+    size_t size;
+    char *buf;
+
+    FILE *fp;
+
+    if ((fp = open_memstream(&buf, &size)) == NULL) {
+        err_sys("open_memstream failed.");
+    }
+
+    for (i = 0; i < 10; i++) {
+        fprintf(fp, "hello, world");
+        printf("current buf address: %p, current buf size: %ld, current buf content:%s\n", buf, (long)strlen(buf), buf);
+        fflush(fp);
+        printf("fflush buf address: %p, current buf size: %ld, current buf content:%s\n", buf, (long)strlen(buf), buf);
+    }
+    return (0);
+}
